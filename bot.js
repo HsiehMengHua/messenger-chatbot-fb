9b5cf23780a810844e70f020c280c73e42a88b0b
Hsieh Meng Hua <qprs71@gmail.com>
2017-09-28 17:34:25 +0800

    -

--- bot.js
+++ bot.js
@@ -7,10 +7,10 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const path = require('path');
 var messengerButton = "<html><head><title>Facebook Messenger Bot</title></head><body><h1>Facebook Messenger Bot (YaYaYa)</h1>This is a bot based on Messenger Platform QuickStart. For more details, see their <a href=\"https://developers.facebook.com/docs/messenger-platform/guides/quick-start\">docs</a>.<script src=\"https://button.glitch.me/button.js\" data-style=\"glitch\"></script><div class=\"glitchButton\" style=\"position:fixed;top:20px;right:20px;\"></div></body></html>";
-var json_joke_1 = require('./joke_1.json');
+/*var json_joke_1 = require('./joke_1.json');
 var obj_joke_1 = JSON.parse(json_joke_1);
 console.log("json_joke_1 = " + json_joke_1);
-console.log("obj_joke_1 = " + obj_joke_1);
+console.log("obj_joke_1 = " + obj_joke_1);*/
 
 
 // The rest of the code implements the routes for our Express server.
